2c3a28aa3761a1c0cec52435bf0a6fe548b7691d
StunnaDawg <jonsonallen9@gmail.com>
2023-11-24 18:20:52 -0400

    Starting the timer, almost there. CLI does not start timer yet

--- Sources/PomodoroTimer.swift
+++ Sources/PomodoroTimer.swift
@@ -5,12 +5,30 @@
 //  Created by Jonson Allen on 2023-11-22.
 //
 
-import Figlet
+import Foundation
 
 @main
 
-struct FigletTool {
-    static func main () {
-        Figlet.say("Swift Pomodero Timer")
+struct TimerApp {
+    
+    static func main() {
+        print("Please desired Timer:")
+        var countdown = 0
+        
+        if let timer = readLine() {
+            print("\(timer) Timer will start in...")
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                print("\(countdown)...")
+                countdown += 1
+                 
+                if countdown == 3 {
+                    print("Start!")
+                    timer.invalidate()
+                }
+            }
+        } else {
+            print("Why are you being so coy?")
+        }
     }
+
 }
